d3e58530b2b3c72302299a0335521635828161f4
SHivanshu <spacester7563@gmail.com>
2025-04-23 11:31:49 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,62 @@
+//BRAND
+export const name = 'Academy';
+export const company = 'Academy';
+export const websiteURL = 'https://academy.pnacorp.co.uk/';
+export const serverURL = 'http://localhost:5000';
+export const logo = 'https://academy.pnacorp.co.uk/logo192.png';
+export const razorpayEnabled = false;
+export const paypalEnabled = false;
+export const stripeEnabled = true;
+export const paystackEnabled = false;
+export const flutterwaveEnabled = false;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 29;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 229;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Jean Byrd";
+export const profession = 'Academy';
+export const photoURL = 'https://academy.pnacorp.co.uk/logo192.png';
+
+//PAYPAL
+export const paypalPlanIdOne = "P-1EM732768S920784HMWKW3OA";
+export const paypalPlanIdTwo = "P-8T744865W27080359MWOCE5Q";
+
+//RAZORPAY
+export const razorpayKeyId = "rzp_test_uqALjZHyTyuiOm";
+export const razorpayPlanIdOne = "plan_NMvvtDfznbRp6V";
+export const razorpayPlanIdTwo = "plan_NMRc9HARBQRLWA";
+
+//STRIPE
+export const stripePlanIdOne = "price_1RGFynEpJzzX5aStXeLrvv9O";
+export const stripePlanIdTwo = "price_1RGG0YEpJzzX5aSt7saEH0JM";
+
+//PAYSTACK
+export const paystackPlanIdOne = "PLN_ouqmm8eo6i2k9k8";
+export const paystackPlanIdTwo = "PLN_1v1xqb8io9t5lis";
+export const amountInZarOne = '170';
+export const amountInZarTwo = '1871';
+
+//FLUTTERWAVE
+export const flutterwavePlanIdOne = "67960";
+export const flutterwavePlanIdTwo = "67961";
+export const flutterwavePublicKey = "FLWPUBK_TEST-6ee1faf6460ea587f510a024ac4c2b23-X";
+
+//SOCIAL SIGNIN
+export const googleClientId = "150644331076-flt197jvuqg9ohlf6q9rsjk3e3qomjd2.apps.googleusercontent.com";
+export const facebookClientId = "8065327730213158";
